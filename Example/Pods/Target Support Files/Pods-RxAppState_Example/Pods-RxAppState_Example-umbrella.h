0cfcf2c928af52f3f0fa2c7528235581d8023ffb
Kasper Welner <kasperwelner@msn.com>
2017-05-31 14:23:10 +0200

    Updated to carthage

--- Example/Pods/Target Support Files/Pods-RxAppState_Example/Pods-RxAppState_Example-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_RxAppState_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RxAppState_ExampleVersionString[];
-
